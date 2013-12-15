967c03bac131c746d74f5883ff3baa5872a053df
Brett J Crawford <brettjamescrawford@gmail.com>
2013-12-15 15:43:36 -0500

    changes to make better use of the mvc pattern

--- src/bc_main/BC_Controller.java
+++ src/bc_main/BC_Controller.java
@@ -2,6 +2,7 @@ package bc_main;
 
 import bc_binarysearchtree.BC_BSTNode;
 import bc_gamenode.BC_GameNode;
+import bc_listnode.BC_ListNode;
 import simplegui.*;
 
 /**
@@ -20,9 +21,6 @@ public class BC_Controller implements SGMouseListener  {
 
     @Override
     public void reactToMouseClick(int x, int y) {
-        if(BC_BSTPuzzle.DEBUG)
-            System.out.println("Mouse x,y: (" + x + "," + y + ")");
-        
         if(model.isStartMode()) {
             handleStartModeClick(x, y);
         }
@@ -32,7 +30,6 @@ public class BC_Controller implements SGMouseListener  {
         else if(model.isEndMode()) {
             handleEndModeClick(x, y);
         }
-        
     }
     
     private void handleStartModeClick(int x, int y) {
@@ -60,86 +57,48 @@ public class BC_Controller implements SGMouseListener  {
             
         if(gameNode != null) { // Game node mouse click
 
-            if(gameNode.getClass() == BC_BSTNode.class) { // Game board mouse click
+            if(gameNode.getClass() == BC_ListNode.class) { // Game piece mouse click
 
-                if(model.isPieceSelected()) { // Input game piece
+                if(!model.isPieceSelected()) { // No game piece selected? Select current game piece
                     
-                    BC_GameNode selectedNode = model.getPieceSelected();
-                    String guiLabelIns = "INS" + gameNode.getNodeIndex();
-                    view.drawNode(selectedNode.getData(), gameNode.getScreenX(), gameNode.getScreenY(), guiLabelIns);
-                    model.getGuiLabelList().push(guiLabelIns);
-                    model.getGameBoard()[gameNode.getNodeIndex()] = selectedNode.getNodeIndex();
-                    model.getGameSpaces()[gameNode.getNodeIndex()] = true;
-                    int nodeX = selectedNode.getScreenX();
-                    int nodeY = selectedNode.getScreenY();
-                    String guiLabelClr = "CLR" + gameNode.getNodeIndex();
-                    view.drawBlankNode(nodeX, nodeY, guiLabelClr);
-                    model.getGuiLabelList().push(guiLabelClr);
-                    model.getGameSpaces()[selectedNode.getNodeIndex() + 31] = false;
-                    view.clearSelection();
-                    model.setPieceSelected(false);
-                    model.setPieceSelectedNode(null);
-                    
-                    if(BC_BSTPuzzle.DEBUG) {
-                        
-                        System.out.println("input game piece");
-                        System.out.println("BST: " + gameNode);
-                        System.out.println("X: " + nodeX + " | Y: " + nodeY);
-                        System.out.println(guiLabelIns + " | " + guiLabelClr);
+                    if(model.isGamePieceAvailable(gameNode.getNodeIndex())) {
+                        view.drawSelection(gameNode);
+                        model.setPieceSelected(true, gameNode);
                     }
+                }
+                else  // Game piece already selected? Clear game piece selection
+                    clearSelection();
+            }
+            else { // Game board mouse click
 
+                if(model.isPieceSelected()) { // Game piece placed
+                    
+                    if(model.isBoardSpotAvailable(gameNode.getNodeIndex())) {
+                        BC_GameNode selectedNode = model.getPieceSelected();
+                        model.insertGuess(gameNode.getNodeIndex(), selectedNode.getNodeIndex());
+                        model.moveUpdate(gameNode.getNodeIndex(), selectedNode.getNodeIndex());
+                        view.drawNode(selectedNode.getData(), gameNode.getScreenX(), gameNode.getScreenY(), gameNode.getNodeIndex());
+                        view.drawBlankNode(selectedNode.getScreenX(), selectedNode.getScreenY(), gameNode.getNodeIndex());
+                    }
+                    clearSelection();
                 }
                 else { // Clear game board spot
                     
                     if(model.getGameBoard()[gameNode.getNodeIndex()] != -1) {
                         
                         view.clearMove("INS" + gameNode.getNodeIndex(), "CLR" + gameNode.getNodeIndex());
-                        model.getGameSpaces()[model.getGameBoard()[gameNode.getNodeIndex()]] = false;
-                        model.getGameSpaces()[model.getGameBoard()[gameNode.getNodeIndex()] + 31] = true;
-                        model.getGameBoard()[gameNode.getNodeIndex()] = -1;
-                        
-                        if(BC_BSTPuzzle.DEBUG) {
-                            
-                            System.out.println("filled node");
-                            System.out.println("INS" + gameNode.getNodeIndex() + " | CLR" + gameNode.getNodeIndex());
-                        }
+                        model.clearGameBoardSpot(gameNode.getNodeIndex());
                     }
-                }
-            }
-            else { // Game piece mouse click
-
-                if(model.isPieceSelected()) { // Clear game piece selection
-                    
-                    view.clearSelection();
-                    model.setPieceSelected(false);
-                    model.setPieceSelectedNode(null);
-                }
-                else { // Select game piece 
-                    
-                    if(model.getGameSpaces()[gameNode.getNodeIndex() + 31]) {
-                        view.drawSelection(gameNode);
-                        model.setPieceSelected(true);
-                        model.setPieceSelectedNode(gameNode);
-                    }
-                    
-                    if(BC_BSTPuzzle.DEBUG)
-                        System.out.println("gameSpaces[" + (gameNode.getNodeIndex() + 31) + "]: " + model.getGameSpaces()[gameNode.getNodeIndex() + 31]);
-                }
-                
-                if(BC_BSTPuzzle.DEBUG) {
-                    System.out.println("SHF: " + gameNode);
                 }
             }
         }
         else { // Blank space mouse click
             
-            view.clearSelection();
-            model.setPieceSelected(false);
-            model.setPieceSelectedNode(null);
+            clearSelection();
 
             // Check for button click
             if(x >= 455 && x <= 624 && y >= 346 && y <= 399) { // Reset 
-                model.resetGameBoard(view.getGui());
+                model.resetGameBoard();
                 view.resetGameBoard(model.getGuiLabelList());
             }
             else if(x >= 455 && x <= 624 && y >= 410 && y <= 462) { // Quit
@@ -160,6 +119,12 @@ public class BC_Controller implements SGMouseListener  {
         model.setEndMode(false);
         model.setGameOver(true);
         view.clearAll();
+    }
+    
+    private void clearSelection() {
+        
+        view.clearSelection();
+        model.setPieceSelected(false, null);
     }
 
 }
